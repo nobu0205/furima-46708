b79a99de162d54745feb951643fbd1bd45bf3f14
nobu0205 <nobuhiro.dream@gmail.com>
2026-02-04 19:07:12 +0900

    商品出品機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_NAME_REGEX =  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   validates :username, presence: true
-  validates :first_name, :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角で入力してください" }
+  validates :first_name, :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角で入力してください' }
 
-  VALID_KANA_REGEX =  /\A[ァ-ヶー]+\z/
-  validates :first_name_kana, :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: "は全角カタカナで入力してください" }
+  VALID_KANA_REGEX = /\A[ァ-ヶー]+\z/
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX, message: 'は全角カタカナで入力してください' }
   validates :birthday, presence: true
 
-  VALID_PASSWORD_REGEX =  /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は英字と数字の両方を含めてください" }, if: -> { password.present? }
-
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字の両方を含めてください' }, if: -> { password.present? }
 end
