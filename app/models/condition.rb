42dd6d12797fb37a1fe96dcb865f85cbab3016a2
nobu0205 <nobuhiro.dream@gmail.com>
2026-02-12 16:02:01 +0900

    商品詳細表示機能の実装

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -10,5 +10,5 @@ class Condition < ActiveHash::Base
   ]
 
   include ActiveHash::Associations
-  has_many :items
+  has_many :items, dependent: :destroy
 end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -51,5 +51,5 @@ class Prefecture < ActiveHash::Base
   ]
 
   include ActiveHash::Associations
-  has_many :items
+  has_many :items, dependent: :destroy
 end
