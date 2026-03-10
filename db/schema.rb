43c0604b766d21525df6be9dfad5069cd1e2bdc3
nobu0205 <nobuhiro.dream@gmail.com>
2026-03-10 19:25:54 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
-  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2026_03_10_095545) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +36,13 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
+  create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", charset: "utf8mb3", force: :cascade do |t|
+  create_table "addresses", force: :cascade do |t|
     t.string "postal_code", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
@@ -52,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
     t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
   end
 
-  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+  create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
@@ -67,7 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "purchases", charset: "utf8mb3", force: :cascade do |t|
+  create_table "purchases", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
@@ -76,7 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username", null: false
@@ -98,6 +101,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_26_084702) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "purchases"
   add_foreign_key "items", "users"
-  add_foreign_key "purchases", "items"
+  add_foreign_key "purchases", "items", on_delete: :cascade
   add_foreign_key "purchases", "users"
 end
