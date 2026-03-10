8d00fd4ea6f69e41ae7c389be08b5c21000700ec
nobu0205 <nobuhiro.dream@gmail.com>
2026-03-10 18:45:34 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  has_one :purchase
+  has_one :purchase, dependent: :destroy
 
   has_one_attached :image
 
